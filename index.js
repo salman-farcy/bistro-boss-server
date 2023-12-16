6548a5ab6a8c9db4016554f4f411912ff058b2a0
salman-farcy <salmanfarcy253@gmail.com>
2023-12-16 19:42:13 +0600

    project sume code

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.rb5g6hh.mongodb.net/?retryWrites=true&w=majority`;
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
      serverApi: {
@@ -24,13 +25,27 @@ const client = new MongoClient(uri, {
 async function run() {
      try {
           // Connect the client to the server	(optional starting in v4.7)
-          await client.connect();
+          await client.connect(); 
+
+          const menuCollection = client.db("BistroBoss").collection("menu");
+          const revewsCollection = client.db("BistroBoss").collection("revews");
+
+          app.get('/menu', async (req, res) => {
+               const result = await menuCollection.find().toArray();
+               res.send(result)
+          })
+
+          app.get('/revews', async (req, res) => {
+               const result = await revewsCollection.find().toArray();
+               res.send(result)
+          })
+
           // Send a ping to confirm a successful connection
           await client.db("admin").command({ ping: 1 });
           console.log("Pinged your deployment. You successfully connected to MongoDB!");
      } finally {
           // Ensures that the client will close when you finish/error
-          await client.close();
+          // await client.close();
      }
 }
 run().catch(console.dir);
