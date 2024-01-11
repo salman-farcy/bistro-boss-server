fc214632c4962e23bec11e376fc0e27f72d4c489
salman-farcy <salmanfarcy253@gmail.com>
2024-01-11 19:15:59 +0600

    The data of dynamic route is not coming properly, I will solve this problem later

--- index.js
+++ index.js
@@ -144,6 +144,14 @@ async function run() {
                res.send(result)
           })
 
+          //
+          app.get('/menu/:id',verifToken, verifyAdmin, async (req, res) => {
+               const id = req.params.id;
+               const query = {_id: new ObjectId(id)}
+               const result = await menuCollection.findOne(query)
+               res.send(result);
+          })
+
           //menu Post 
           app.post('/menu', verifToken, verifyAdmin, async (req, res) => {
                const item = req.body;
