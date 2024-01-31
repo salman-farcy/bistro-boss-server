6eee05491523f23400b9a1ae514c39569df074d8
salman-farcy <salmanfarcy253@gmail.com>
2024-02-01 02:12:00 +0600

    aggregate use and add  pipe line

--- index.js
+++ index.js
@@ -283,6 +283,38 @@ async function run() {
           })
 
 
+          //Order Status
+          //using aggregate pipeline
+
+          app.get('/order-stats', async (req, res) => {
+               const result = await paymentCollection.aggregate([
+                    {
+                         $unwind: '$menuItemIds'
+                    },
+                    {
+                         $lookup: {
+                              from:  'menu',
+                              localField: 'menuItemIds',
+                              foreignField: '_id',
+                              as: 'menuItems'
+                         }
+                    },
+                    {
+                         $unwind: '$menuItems'
+                    },
+                    {
+                         $group: {
+                              _id: '$menuItems.category',
+                              quantity: { $sum: 1 },
+                              revenue: {$sum: '$menuItems.price'}
+                         }
+                    }
+               ]).toArray()
+
+               res.send(result)
+          })
+
+
           // Send a ping to confirm a successful connection
           await client.db("admin").command({ ping: 1 });
           console.log("Pinged your deployment. You successfully connected to MongoDB!");
